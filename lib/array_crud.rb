8ea886640fecb21e2a7b64b8057f216eb2476797
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-06 21:33:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["one", "two", "three", "four"]
+  ["beans", "rice", "spice", "so nice!"]
 end
 
 def add_element_to_end_of_array(array, element)
